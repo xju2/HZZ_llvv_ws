ba38e891cabdc2107f96bb031b8a5348a1040245
xiangyang.ju <xiangyang.ju@cern.ch>
2017-06-05 13:35:11 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='HZZ_llvv_ws',
+      version='0.1',
+      description='Script to make workspace',
+      url='https://github.com/xju2/HZZ_llvv_ws',
+      author='Xiangyang Ju',
+      author_email='xiangyang.ju@gmail.com',
+      license='MIT',
+      packages=['HZZ_llvv_ws'],
+      zip_safe=False
+     )
